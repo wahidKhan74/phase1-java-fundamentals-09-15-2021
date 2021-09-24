7d8e91428f294eb5f0614baac5bc75343c8b76fb
Waheed Khan <wahid.khan74@gmail.com>
2021-09-24 07:22:42 +0000

    add multi threading

---
+++ src/com/simplilearn/collections/multithreading/AnonymousThreadWithLambda.java
@@ -0,0 +1,26 @@
+package com.simplilearn.collections.multithreading;
+
+public class AnonymousThreadWithLambda {
+
+	public static void main(String[] args) {
+
+		// create a thread for will
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 5; i++) {
+				System.out.println("Wills says : Hello");
+			}
+		});
+
+		// create a thread for bob
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Bob says : Hi");
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+	}
+
+}
